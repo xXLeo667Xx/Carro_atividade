e467f39e7bf65614d1221681365e4880c481f25e
xXLeo667Xx <127798966+xXLeo667Xx@users.noreply.github.com>
2023-04-24 20:00:17 -0300

    Update classe_carro.cpp

--- classe_carro.cpp
+++ classe_carro.cpp
@@ -8,11 +8,19 @@ class carro{
     string modelo;
     float VelMax;
     void Buzinar();
+    //contrutor ja definito para não precisar ficar instanciando no programa principal
+    carro(){
+    VelMax = 220.f;
+    modelo = "Camaro";
+    cor = "Amarelo";
+   }
 };
 
 int main(){
+    
     //criando as novas variaveis do carro
     carro MeuCarro;
+    carro carro2;
      MeuCarro.cor="Branco";
      MeuCarro.modelo="Fusca";
      MeuCarro.VelMax=150.0f;
@@ -21,6 +29,11 @@ int main(){
      cout<<MeuCarro.cor<<endl;
      cout<<MeuCarro.modelo<<endl;
      cout<<MeuCarro.VelMax<<endl;
+     //interface que mostrara o resultado do nosso constrtutor camaro ja colocado na classe para n precisar ficar instanciando
+     carro2.Buzinar();
+     cout << carro2.cor << endl;
+     cout << carro2.VelMax << endl;
+     cout << carro2.modelo << endl;
 }
 //interface de usuario do modulo 
 void carro::Buzinar(){
